c7e5e929768cfa1a677a4ced1859e61019244748
Pedro <salazar9_5@hotmail.com>
2025-02-17 08:20:54 -0600

    gauss labels results

--- intro/Gauss.Designer.cs
+++ intro/Gauss.Designer.cs
@@ -28,12 +28,385 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tb_11 = new System.Windows.Forms.TextBox();
+            this.tn_12 = new System.Windows.Forms.TextBox();
+            this.tb_13 = new System.Windows.Forms.TextBox();
+            this.tb_14 = new System.Windows.Forms.TextBox();
+            this.tb_21 = new System.Windows.Forms.TextBox();
+            this.tb_22 = new System.Windows.Forms.TextBox();
+            this.tb_23 = new System.Windows.Forms.TextBox();
+            this.tb_24 = new System.Windows.Forms.TextBox();
+            this.tb_31 = new System.Windows.Forms.TextBox();
+            this.tb_32 = new System.Windows.Forms.TextBox();
+            this.tb_33 = new System.Windows.Forms.TextBox();
+            this.tb_34 = new System.Windows.Forms.TextBox();
+            this.tb_44 = new System.Windows.Forms.TextBox();
+            this.tb_43 = new System.Windows.Forms.TextBox();
+            this.tb_42 = new System.Windows.Forms.TextBox();
+            this.tb_41 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lb_11 = new System.Windows.Forms.Label();
+            this.lb_12 = new System.Windows.Forms.Label();
+            this.lb_13 = new System.Windows.Forms.Label();
+            this.lb_14 = new System.Windows.Forms.Label();
+            this.lb_21 = new System.Windows.Forms.Label();
+            this.lb_22 = new System.Windows.Forms.Label();
+            this.lb_23 = new System.Windows.Forms.Label();
+            this.lb_24 = new System.Windows.Forms.Label();
+            this.lb_31 = new System.Windows.Forms.Label();
+            this.lb_32 = new System.Windows.Forms.Label();
+            this.lb_33 = new System.Windows.Forms.Label();
+            this.lb_34 = new System.Windows.Forms.Label();
+            this.lb_41 = new System.Windows.Forms.Label();
+            this.lb_42 = new System.Windows.Forms.Label();
+            this.lb_43 = new System.Windows.Forms.Label();
+            this.lb_44 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_11
+            // 
+            this.tb_11.Location = new System.Drawing.Point(12, 12);
+            this.tb_11.Name = "tb_11";
+            this.tb_11.Size = new System.Drawing.Size(67, 20);
+            this.tb_11.TabIndex = 0;
+            // 
+            // tn_12
+            // 
+            this.tn_12.Location = new System.Drawing.Point(92, 12);
+            this.tn_12.Name = "tn_12";
+            this.tn_12.Size = new System.Drawing.Size(66, 20);
+            this.tn_12.TabIndex = 1;
+            // 
+            // tb_13
+            // 
+            this.tb_13.Location = new System.Drawing.Point(164, 12);
+            this.tb_13.Name = "tb_13";
+            this.tb_13.Size = new System.Drawing.Size(68, 20);
+            this.tb_13.TabIndex = 2;
+            // 
+            // tb_14
+            // 
+            this.tb_14.Location = new System.Drawing.Point(238, 12);
+            this.tb_14.Name = "tb_14";
+            this.tb_14.Size = new System.Drawing.Size(67, 20);
+            this.tb_14.TabIndex = 3;
+            // 
+            // tb_21
+            // 
+            this.tb_21.Location = new System.Drawing.Point(12, 38);
+            this.tb_21.Name = "tb_21";
+            this.tb_21.Size = new System.Drawing.Size(67, 20);
+            this.tb_21.TabIndex = 4;
+            // 
+            // tb_22
+            // 
+            this.tb_22.Location = new System.Drawing.Point(92, 38);
+            this.tb_22.Name = "tb_22";
+            this.tb_22.Size = new System.Drawing.Size(66, 20);
+            this.tb_22.TabIndex = 5;
+            // 
+            // tb_23
+            // 
+            this.tb_23.Location = new System.Drawing.Point(164, 38);
+            this.tb_23.Name = "tb_23";
+            this.tb_23.Size = new System.Drawing.Size(68, 20);
+            this.tb_23.TabIndex = 6;
+            // 
+            // tb_24
+            // 
+            this.tb_24.Location = new System.Drawing.Point(238, 38);
+            this.tb_24.Name = "tb_24";
+            this.tb_24.Size = new System.Drawing.Size(67, 20);
+            this.tb_24.TabIndex = 7;
+            // 
+            // tb_31
+            // 
+            this.tb_31.Location = new System.Drawing.Point(12, 64);
+            this.tb_31.Name = "tb_31";
+            this.tb_31.Size = new System.Drawing.Size(67, 20);
+            this.tb_31.TabIndex = 8;
+            // 
+            // tb_32
+            // 
+            this.tb_32.Location = new System.Drawing.Point(92, 64);
+            this.tb_32.Name = "tb_32";
+            this.tb_32.Size = new System.Drawing.Size(66, 20);
+            this.tb_32.TabIndex = 9;
+            // 
+            // tb_33
+            // 
+            this.tb_33.Location = new System.Drawing.Point(164, 64);
+            this.tb_33.Name = "tb_33";
+            this.tb_33.Size = new System.Drawing.Size(68, 20);
+            this.tb_33.TabIndex = 10;
+            // 
+            // tb_34
+            // 
+            this.tb_34.Location = new System.Drawing.Point(238, 64);
+            this.tb_34.Name = "tb_34";
+            this.tb_34.Size = new System.Drawing.Size(67, 20);
+            this.tb_34.TabIndex = 11;
+            // 
+            // tb_44
+            // 
+            this.tb_44.Location = new System.Drawing.Point(238, 90);
+            this.tb_44.Name = "tb_44";
+            this.tb_44.Size = new System.Drawing.Size(67, 20);
+            this.tb_44.TabIndex = 15;
+            // 
+            // tb_43
+            // 
+            this.tb_43.Location = new System.Drawing.Point(164, 90);
+            this.tb_43.Name = "tb_43";
+            this.tb_43.Size = new System.Drawing.Size(68, 20);
+            this.tb_43.TabIndex = 14;
+            // 
+            // tb_42
+            // 
+            this.tb_42.Location = new System.Drawing.Point(92, 90);
+            this.tb_42.Name = "tb_42";
+            this.tb_42.Size = new System.Drawing.Size(66, 20);
+            this.tb_42.TabIndex = 13;
+            // 
+            // tb_41
+            // 
+            this.tb_41.Location = new System.Drawing.Point(12, 90);
+            this.tb_41.Name = "tb_41";
+            this.tb_41.Size = new System.Drawing.Size(67, 20);
+            this.tb_41.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lb_11
+            // 
+            this.lb_11.AutoSize = true;
+            this.lb_11.Location = new System.Drawing.Point(51, 177);
+            this.lb_11.Name = "lb_11";
+            this.lb_11.Size = new System.Drawing.Size(16, 13);
+            this.lb_11.TabIndex = 17;
+            this.lb_11.Text = "---";
+            // 
+            // lb_12
+            // 
+            this.lb_12.AutoSize = true;
+            this.lb_12.Location = new System.Drawing.Point(106, 177);
+            this.lb_12.Name = "lb_12";
+            this.lb_12.Size = new System.Drawing.Size(16, 13);
+            this.lb_12.TabIndex = 18;
+            this.lb_12.Text = "---";
+            // 
+            // lb_13
+            // 
+            this.lb_13.AutoSize = true;
+            this.lb_13.Location = new System.Drawing.Point(165, 177);
+            this.lb_13.Name = "lb_13";
+            this.lb_13.Size = new System.Drawing.Size(16, 13);
+            this.lb_13.TabIndex = 19;
+            this.lb_13.Text = "---";
+            // 
+            // lb_14
+            // 
+            this.lb_14.AutoSize = true;
+            this.lb_14.Location = new System.Drawing.Point(225, 177);
+            this.lb_14.Name = "lb_14";
+            this.lb_14.Size = new System.Drawing.Size(16, 13);
+            this.lb_14.TabIndex = 20;
+            this.lb_14.Text = "---";
+            // 
+            // lb_21
+            // 
+            this.lb_21.AutoSize = true;
+            this.lb_21.Location = new System.Drawing.Point(51, 209);
+            this.lb_21.Name = "lb_21";
+            this.lb_21.Size = new System.Drawing.Size(16, 13);
+            this.lb_21.TabIndex = 21;
+            this.lb_21.Text = "---";
+            // 
+            // lb_22
+            // 
+            this.lb_22.AutoSize = true;
+            this.lb_22.Location = new System.Drawing.Point(106, 209);
+            this.lb_22.Name = "lb_22";
+            this.lb_22.Size = new System.Drawing.Size(16, 13);
+            this.lb_22.TabIndex = 22;
+            this.lb_22.Text = "---";
+            // 
+            // lb_23
+            // 
+            this.lb_23.AutoSize = true;
+            this.lb_23.Location = new System.Drawing.Point(161, 209);
+            this.lb_23.Name = "lb_23";
+            this.lb_23.Size = new System.Drawing.Size(16, 13);
+            this.lb_23.TabIndex = 23;
+            this.lb_23.Text = "---";
+            // 
+            // lb_24
+            // 
+            this.lb_24.AutoSize = true;
+            this.lb_24.Location = new System.Drawing.Point(222, 209);
+            this.lb_24.Name = "lb_24";
+            this.lb_24.Size = new System.Drawing.Size(16, 13);
+            this.lb_24.TabIndex = 24;
+            this.lb_24.Text = "---";
+            // 
+            // lb_31
+            // 
+            this.lb_31.AutoSize = true;
+            this.lb_31.Location = new System.Drawing.Point(51, 242);
+            this.lb_31.Name = "lb_31";
+            this.lb_31.Size = new System.Drawing.Size(16, 13);
+            this.lb_31.TabIndex = 25;
+            this.lb_31.Text = "---";
+            // 
+            // lb_32
+            // 
+            this.lb_32.AutoSize = true;
+            this.lb_32.Location = new System.Drawing.Point(106, 242);
+            this.lb_32.Name = "lb_32";
+            this.lb_32.Size = new System.Drawing.Size(16, 13);
+            this.lb_32.TabIndex = 26;
+            this.lb_32.Text = "---";
+            // 
+            // lb_33
+            // 
+            this.lb_33.AutoSize = true;
+            this.lb_33.Location = new System.Drawing.Point(161, 242);
+            this.lb_33.Name = "lb_33";
+            this.lb_33.Size = new System.Drawing.Size(16, 13);
+            this.lb_33.TabIndex = 27;
+            this.lb_33.Text = "---";
+            // 
+            // lb_34
+            // 
+            this.lb_34.AutoSize = true;
+            this.lb_34.Location = new System.Drawing.Point(219, 242);
+            this.lb_34.Name = "lb_34";
+            this.lb_34.Size = new System.Drawing.Size(16, 13);
+            this.lb_34.TabIndex = 28;
+            this.lb_34.Text = "---";
+            // 
+            // lb_41
+            // 
+            this.lb_41.AutoSize = true;
+            this.lb_41.Location = new System.Drawing.Point(51, 274);
+            this.lb_41.Name = "lb_41";
+            this.lb_41.Size = new System.Drawing.Size(16, 13);
+            this.lb_41.TabIndex = 29;
+            this.lb_41.Text = "---";
+            // 
+            // lb_42
+            // 
+            this.lb_42.AutoSize = true;
+            this.lb_42.Location = new System.Drawing.Point(106, 274);
+            this.lb_42.Name = "lb_42";
+            this.lb_42.Size = new System.Drawing.Size(16, 13);
+            this.lb_42.TabIndex = 30;
+            this.lb_42.Text = "---";
+            // 
+            // lb_43
+            // 
+            this.lb_43.AutoSize = true;
+            this.lb_43.Location = new System.Drawing.Point(161, 274);
+            this.lb_43.Name = "lb_43";
+            this.lb_43.Size = new System.Drawing.Size(16, 13);
+            this.lb_43.TabIndex = 31;
+            this.lb_43.Text = "---";
+            // 
+            // lb_44
+            // 
+            this.lb_44.AutoSize = true;
+            this.lb_44.Location = new System.Drawing.Point(222, 274);
+            this.lb_44.Name = "lb_44";
+            this.lb_44.Size = new System.Drawing.Size(16, 13);
+            this.lb_44.TabIndex = 32;
+            this.lb_44.Text = "---";
+            // 
+            // Gauss
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(358, 335);
+            this.Controls.Add(this.lb_44);
+            this.Controls.Add(this.lb_43);
+            this.Controls.Add(this.lb_42);
+            this.Controls.Add(this.lb_41);
+            this.Controls.Add(this.lb_34);
+            this.Controls.Add(this.lb_33);
+            this.Controls.Add(this.lb_32);
+            this.Controls.Add(this.lb_31);
+            this.Controls.Add(this.lb_24);
+            this.Controls.Add(this.lb_23);
+            this.Controls.Add(this.lb_22);
+            this.Controls.Add(this.lb_21);
+            this.Controls.Add(this.lb_14);
+            this.Controls.Add(this.lb_13);
+            this.Controls.Add(this.lb_12);
+            this.Controls.Add(this.lb_11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_44);
+            this.Controls.Add(this.tb_43);
+            this.Controls.Add(this.tb_42);
+            this.Controls.Add(this.tb_41);
+            this.Controls.Add(this.tb_34);
+            this.Controls.Add(this.tb_33);
+            this.Controls.Add(this.tb_32);
+            this.Controls.Add(this.tb_31);
+            this.Controls.Add(this.tb_24);
+            this.Controls.Add(this.tb_23);
+            this.Controls.Add(this.tb_22);
+            this.Controls.Add(this.tb_21);
+            this.Controls.Add(this.tb_14);
+            this.Controls.Add(this.tb_13);
+            this.Controls.Add(this.tn_12);
+            this.Controls.Add(this.tb_11);
+            this.Name = "Gauss";
             this.Text = "Gauss";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tb_11;
+        private System.Windows.Forms.TextBox tn_12;
+        private System.Windows.Forms.TextBox tb_13;
+        private System.Windows.Forms.TextBox tb_14;
+        private System.Windows.Forms.TextBox tb_21;
+        private System.Windows.Forms.TextBox tb_22;
+        private System.Windows.Forms.TextBox tb_23;
+        private System.Windows.Forms.TextBox tb_24;
+        private System.Windows.Forms.TextBox tb_31;
+        private System.Windows.Forms.TextBox tb_32;
+        private System.Windows.Forms.TextBox tb_33;
+        private System.Windows.Forms.TextBox tb_34;
+        private System.Windows.Forms.TextBox tb_44;
+        private System.Windows.Forms.TextBox tb_43;
+        private System.Windows.Forms.TextBox tb_42;
+        private System.Windows.Forms.TextBox tb_41;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lb_11;
+        private System.Windows.Forms.Label lb_12;
+        private System.Windows.Forms.Label lb_13;
+        private System.Windows.Forms.Label lb_14;
+        private System.Windows.Forms.Label lb_21;
+        private System.Windows.Forms.Label lb_22;
+        private System.Windows.Forms.Label lb_23;
+        private System.Windows.Forms.Label lb_24;
+        private System.Windows.Forms.Label lb_31;
+        private System.Windows.Forms.Label lb_32;
+        private System.Windows.Forms.Label lb_33;
+        private System.Windows.Forms.Label lb_34;
+        private System.Windows.Forms.Label lb_41;
+        private System.Windows.Forms.Label lb_42;
+        private System.Windows.Forms.Label lb_43;
+        private System.Windows.Forms.Label lb_44;
     }
 }
